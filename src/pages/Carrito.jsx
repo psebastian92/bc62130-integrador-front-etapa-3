16a250767c183dfa9de365b34be97dd76e2a0cf5
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-10-16 00:20:59 -0300

    Armo la estructura del carrito en su página correspondiente

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,8 +1,18 @@
+import { useContext } from "react";
 import "./Carrito.scss";
+import CarritoContext from "../contexts/CarritoContext";
+
 const Carrito = () => {
+  const { carrito, eliminarCarritoContext } = useContext(CarritoContext);
+
+  const handleEliminar = (id) => {
+    //console.log(id)
+    eliminarCarritoContext(id);
+  };
+
   return (
     <>
-    <h1>Listado de productos en el carrito</h1>
+      <h1>Listado de productos en el carrito</h1>
       <table className="tabla-carrito">
         <thead>
           <tr>
@@ -14,15 +24,30 @@ const Carrito = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>foto</td>
-            <td>nombre</td>
-            <td>cantidad</td>
-            <td>precio</td>
-            <td>acciones</td>
-          </tr>
+          {carrito.length <= 0 ? (
+            <tr>
+              <td colSpan={5}>
+                <strong>No hay productos</strong>
+              </td>
+            </tr>
+          ) : (
+            carrito.map((producto, idx) => (
+              <tr key={idx}>
+                <td>
+                  <img src={producto.foto} alt={producto.nombre} width="50px" />
+                </td>
+                <td>{producto.nombre}</td>
+                <td>{producto.cantidad}</td>
+                <td>{producto.precio}</td>
+                <td>
+                  <button onClick={() => handleEliminar(producto.id)}>
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
-        <tbody></tbody>
       </table>
     </>
   );
