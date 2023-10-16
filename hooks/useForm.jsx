5cc31968e4138da375bb4c2e7ff6d12fb134d80f
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-10-15 23:27:13 -0300

    Agrego Hooks

---
+++ hooks/useForm.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from "react"
+
+export const useForm = ( estadoInicial = {} ) => {
+    const [values, setValues] = useState(estadoInicial)
+
+    const handleInputChange = (e) => {
+        const { type, name, checked, value } = e.target
+        setValues({
+          ...values,
+          [name]: type === 'checkbox' ? checked : value
+        })
+      }
+
+    return [ values, setValues, handleInputChange ]
+
+}
