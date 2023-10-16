b88ae51c168c34bc99c7750b38a731db09a8330f
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-10-15 21:07:44 -0300

    Agrego el icono del carrito, y el de la tienda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,52 @@
-import { Link } from 'react-router-dom'
-import './Header.scss'
-import Navbar from './Navbar'
+  import { Link } from "react-router-dom";
+  import "./Header.scss";
+  import Navbar from "./Navbar";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
+  import { faMicrochip } from "@fortawesome/free-solid-svg-icons"
 
-const Header = () => {
-  return (
-    <header className="main-header">
-    <input type="checkbox" id="menu"/>
+  const Header = () => {
+    return (
+      <header className="main-header">
+        <input type="checkbox" id="menu" />
+        <Navbar />
+        <div className="search-bar">
+          {" "}
+          {/*<!-- search-bar --> */}
+          <div className="search-bar__logo-container">
+          <FontAwesomeIcon icon={faMicrochip} className="search-bar__logo-img" />
+          </div>
+          <form action="" className="search-bar__form-container">
+            <label htmlFor="busqueda" className="search-bar__htmlForm-label">
+              Buscar
+            </label>
+            <input
+              type="search"
+              className="search-bar__form-search"
+              id="busqueda"
+            />
+            <input
+              type="submit"
+              value="Buscar"
+              className="search-bar__form-submit"
+            />
+          </form>
+          <div className="search-bar__carrito-container">
+            <Link to="/carrito">
+            <FontAwesomeIcon icon={faShoppingCart} className="search-bar__cart-icon"/>
+            </Link>
+          </div>
+          <div className="menu-toggle">
+            <label htmlFor="menu" className="menu-toggle__label">
+              <span className="menu-toggle__top-bread"></span>
+              <span className="menu-toggle__meat"></span>
+              <span className="menu-toggle__bottom-bread"></span>
+            </label>
+          </div>
+        </div>{" "}
+        {/*<!-- search-bar --> */}
+      </header>
+    );
+  };
 
-    <Navbar/>
-
-     <div className="search-bar"> {/*<!-- search-bar --> */}
-      <div className="search-bar__logo-container">
-        <i className="fa-solid fa-microchip search-bar__logo-img "></i>
-      </div>
-      <form action="" className="search-bar__form-container">
-        <label htmlFor="busqueda" className="search-bar__htmlForm-label">Buscar</label>
-        <input type="search" className="search-bar__form-search" id="busqueda"/>
-        <input type="submit" value="Buscar" className="search-bar__form-submit"/>
-      </form>
-      <Link className="search-bar__carrito-container" to="/carrito">C</Link>
-      <div className="menu-toggle">
-        <label htmlFor="menu" className="menu-toggle__label">
-          <span className="menu-toggle__top-bread"></span>
-          <span className="menu-toggle__meat"></span>
-          <span className="menu-toggle__bottom-bread"></span>
-        </label>
-      </div>
-     </div> {/*<!-- search-bar --> */}
-  </header>
-
-  )
-}
-
-export default Header
+  export default Header;
