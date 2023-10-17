df2ccb7debf494377f20e4f3b851e3e0ee4f050c
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-10-17 00:02:17 -0300

    Modifico el header y el parrafo de la pagina de inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -6,12 +6,14 @@ import ProductoContext from '../contexts/ProductoContext'
 const Inicio = () => {
   const { productos } = useContext(ProductoContext)
 
+  const cantidadProductos = productos ? productos.length : 0;
+
   return (
   <main>
     <section className="section-cards">
       <header className="section-cards__header">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <p>Se encontraron X productos</p>
+        <h1 className='section-cards__header-h1'>ArduShop</h1>
+        <p className='section-cards__header-p'>Se encontraron  <span className='section-cards__header-p__cantproducts'>{cantidadProductos}</span> productos</p>
       </header>
 
       <div className="cards-container">
