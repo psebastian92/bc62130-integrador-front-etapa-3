a430f92d53fd02899a282b86c19f88b9683e546a
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-10-21 16:05:37 -0300

    Edito estilos a la página Nosotros

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -8,43 +8,39 @@ const Nosotros = () => {
     </header>
     <div className="about-us__content">
       <p className="about-us__span">¡Bienvenidos a ArduShop!</p>
-      <p>
-        Somos tu destino confiable y apasionado para todo lo relacionado con la emocionante y creativa mundo de
-        Arduino.
-        Desde 2020, hemos estado brindando soluciones innovadoras y productos de alta calidad en la Ciudad Autónoma de
-        Buenos Aires y más allá. Nos enorgullecemos de ser líderes en la venta de artículos de Arduino, atendiendo tanto
-        a
-        clientes mayoristas como minoristas.
-      </p>
-      <p>
-        Nuestra historia comenzó con la pasión por la electrónica y la programación. Impulsados por el deseo de
-        compartir
-        nuestra fascinación por Arduino, decidimos establecer ArduShop. A lo largo de los años, hemos cultivado
-        relaciones
-        sólidas con fabricantes y proveedores confiables, lo que nos permite ofrecerte una amplia gama de productos que
-        abarcan desde placas Arduino hasta componentes y accesorios especializados.
-      </p>
-      <p>
-        Lo que nos distingue es nuestro compromiso inquebrantable con la satisfacción del cliente. Nos esforzamos por
-        brindar un servicio personalizado que se adapte a tus necesidades específicas, ya sea que estés comenzando tu
-        viaje en el mundo de la electrónica o que seas un experto en Arduino. Nuestro equipo de expertos apasionados
-        está
-        aquí para asesorarte, responder tus preguntas y proporcionarte la orientación necesaria para que tus proyectos
-        se
-        conviertan en realidad.
-      </p>
-      <p>
-        Ya sea que estés buscando expandir tus capacidades creativas, impulsar la educación en STEM o llevar a cabo
-        proyectos innovadores, ArduShop es tu socio confiable en cada paso del camino.
-      </p>
-      <p>
-        Te invitamos a explorar nuestra colección de productos y descubrir cómo Arduino puede abrir un mundo de
-        posibilidades. ¡Gracias por elegirnos como tu fuente confiable para todo lo relacionado con Arduino!
-      </p>
-      <p>
-        ¡Construyamos juntos el futuro, un circuito a la vez!
-      </p>
-      <p>ArduShop</p>
+      <p className="about-us__parrafo">
+          Somos tu destino confiable y apasionado para todo lo relacionado con el emocionante y creativo mundo de Arduino.
+          Desde 2020, hemos estado brindando soluciones innovadoras y productos de alta calidad en la Ciudad Autónoma de
+          Buenos Aires y más allá. Nos enorgullecemos de ser líderes en la venta de artículos de Arduino, atendiendo tanto a
+          clientes mayoristas como minoristas.
+        </p>
+        <p className="about-us__parrafo">
+          Nuestra historia comenzó con la pasión por la electrónica y la programación. Impulsados por el deseo de compartir
+          nuestra fascinación por Arduino, decidimos establecer ArduShop. A lo largo de los años, hemos cultivado
+          relaciones sólidas con fabricantes y proveedores confiables, lo que nos permite ofrecerte una amplia gama de productos que
+          abarcan desde placas Arduino hasta componentes y accesorios especializados.
+        </p>
+        <p className="about-us__parrafo">
+          Lo que nos distingue es nuestro compromiso inquebrantable con la satisfacción del cliente. Nos esforzamos por
+          brindar un servicio personalizado que se adapte a tus necesidades específicas, ya sea que estés comenzando tu
+          viaje en el mundo de la electrónica o que seas un experto en Arduino. Nuestro equipo de expertos apasionados
+          está
+          aquí para asesorarte, responder tus preguntas y proporcionarte la orientación necesaria para que tus proyectos
+          se
+          conviertan en realidad.
+        </p>
+        <p className="about-us__parrafo">
+          Ya sea que estés buscando expandir tus capacidades creativas, impulsar la educación en STEM o llevar a cabo
+          proyectos innovadores, ArduShop es tu socio confiable en cada paso del camino.
+        </p>
+        <p className="about-us__parrafo">
+          Te invitamos a explorar nuestra colección de productos y descubrir cómo Arduino puede abrir un mundo de
+          posibilidades. ¡Gracias por elegirnos como tu fuente confiable para todo lo relacionado con Arduino!
+        </p>
+        <p className="about-us__parrafo">
+          ¡Construyamos juntos el futuro, un circuito a la vez!
+        </p>
+      <p className='about-us__firma'>ArduShop</p>
     </div>
   </section>
   )
