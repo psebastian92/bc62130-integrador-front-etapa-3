85058aef496517f7799d2e005d3d04fa73484259
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-10-21 15:40:51 -0300

    Agrego un contador visible al carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,65 @@
-  import { Link } from "react-router-dom";
-  import "./Header.scss";
-  import Navbar from "./Navbar";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
-  import { faMicrochip } from "@fortawesome/free-solid-svg-icons"
+import { useContext } from 'react'
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import Navbar from "./Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faMicrochip } from "@fortawesome/free-solid-svg-icons";
+import CarritoContext from "../contexts/CarritoContext";
 
-  const Header = () => {
-    return (
-      <header className="main-header">
-        <input type="checkbox" id="menu" />
-        <Navbar />
-        <div className="search-bar">
-          {" "}
-          {/*<!-- search-bar --> */}
-          <div className="search-bar__logo-container">
-          <FontAwesomeIcon icon={faMicrochip} className="search-bar__logo-img" />
-          </div>
-          <form action="" className="search-bar__form-container">
-            <label htmlFor="busqueda" className="search-bar__htmlForm-label">
-              Búsqueda
-            </label>
-            <input
-              type="search"
-              className="search-bar__form-search"
-              id="busqueda"
-            />
-            <input
-              type="submit"
-              value="Buscar"
-              className="search-bar__form-submit"
-            />
-          </form>
-          <div className="search-bar__carrito-container">
-            <Link to="/carrito">
-            <FontAwesomeIcon icon={faShoppingCart} className="search-bar__cart-icon"/>
-            </Link>
-          </div>
-          <div className="menu-toggle">
-            <label htmlFor="menu" className="menu-toggle__label">
-              <span className="menu-toggle__top-bread"></span>
-              <span className="menu-toggle__meat"></span>
-              <span className="menu-toggle__bottom-bread"></span>
-            </label>
-          </div>
-        </div>{" "}
+const Header = () => {
+  const { carrito } = useContext(CarritoContext);
+
+  const contadorCarrito = carrito ? carrito.length : 0;
+
+  return (
+    <header className="main-header">
+      <input type="checkbox" id="menu" />
+      <Navbar />
+      <div className="search-bar">
+        {" "}
         {/*<!-- search-bar --> */}
-      </header>
-    );
-  };
+        <div className="search-bar__logo-container">
+          <FontAwesomeIcon
+            icon={faMicrochip}
+            className="search-bar__logo-img"
+          />
+        </div>
+        <form action="" className="search-bar__form-container">
+          <label htmlFor="busqueda" className="search-bar__htmlForm-label">
+            Búsqueda
+          </label>
+          <input
+            type="search"
+            className="search-bar__form-search"
+            id="busqueda"
+          />
+          <input
+            type="submit"
+            value="Buscar"
+            className="search-bar__form-submit"
+          />
+        </form>
+        <div className="search-bar__carrito-container">
+          <Link to="/carrito">
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              className="search-bar__carrito-icon"
+            />
+          </Link>
+          <div className="search-bar__carrito-contador">{contadorCarrito}</div>
+        </div>
+        <div className="menu-toggle">
+          <label htmlFor="menu" className="menu-toggle__label">
+            <span className="menu-toggle__top-bread"></span>
+            <span className="menu-toggle__meat"></span>
+            <span className="menu-toggle__bottom-bread"></span>
+          </label>
+        </div>
+      </div>{" "}
+      {/*<!-- search-bar --> */}
+    </header>
+  );
+};
 
-  export default Header;
+export default Header;
