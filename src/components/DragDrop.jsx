1ae12c8a592a5392dcdea465496f72a8879d766f
Sebastian Munoz <sebastian.munoz092@gmail.com>
2023-11-27 21:03:38 -0300

    Actualizo detalles front

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -49,7 +49,7 @@ const DragDrop = ({ setFoto, setSrcImagen, srcImagen }) => {
   
     try {
       formData.append('foto', file);
-      const imagenUp = await post('https://etapa-3-ymva.onrender.com/api/productos/upload/', formData);
+      const imagenUp = await post('https://etapa-3-ymva.onrender.com/api/upload', formData);
       console.log('Respuesta del servidor:', imagenUp);
       setFoto(imagenUp);
     } catch (error) {
